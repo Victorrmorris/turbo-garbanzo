197a99853803e4592e9d08bfbefc1663699e72dd
Victor Morris <92633344+Victorrmorris@users.noreply.github.com>
2023-01-24 10:45:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -151,6 +151,9 @@ def main():
     with st.form(key = 'form1', clear_on_submit=False):
         Job_design = st.text_area("Job design and role crafting template data", value=demo_1)
         transferable_knowledge_skills = st.text_input("Choose your transferable knowledge and skills")
+        transferable_attitudes_abilities = st.text_input("Choose your transferable attitudes and abilities")
+        ideal_employee_experience_factors = st.text_input("Choose your ideal civilian employee experience factors")
+        ideal_work_activities = st.text_input("Choose your ideal civilian work activities")
         Job_description1 = st.text_area("First Job description",value=demo_2)
         Job_description2 = st.text_area("Second Job description",value=demo_3)
         Job_description3 = st.text_area("Third Job description",value=demo_4)
